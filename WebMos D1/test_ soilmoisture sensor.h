085bc580423d24e0ac2981ca2721d5ceea6f25f3
RAMA CHIDAMBARAM <rmr.chidambaram@st.niituniversity.in>
2017-01-25 17:06:25 +0530

    Add files via upload

---
+++ WebMos D1/test_ soilmoisture sensor.h
@@ -0,0 +1,42 @@
+
+/* # the sensor value description
+# 0  ~300     dry soil
+# 300~700     humid soil
+# 700~950     in water*/
+
+
+#include <ESP8266WiFi.h>
+
+	int sensor pin = A0;	//assumption
+	int sensor_value;  
+	int delay = 500;	// delay to boot up.
+
+
+int soil_moisture() 
+{
+	soil_value=analogRead(A0);  // read from analog pin A0
+
+ 	Serial.print(soil_value);
+
+if(soil_value<300)
+{
+
+	
+	return 0;
+}
+
+else if((soil_value>300)&&(soil_value<700))
+{
+
+	
+	return 1;
+}
+
+else if((soil_value>700)&&(soil_value<950))
+{
+
+	return 2;
+}
+call(delay);
+}
+
